cb6a14ec7d2bfb48f5aa048bdcb94b97d662b43d
linusmbae <linusmutugi5178@gmail.com>
2020-12-13 15:00:27 +0300

    save changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ var countdownfunction = setInterval(function() {
   var distance = countDownDate - now;
   
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (10000 * 60 * 60 * 24));
+  var days = Math.floor(distance / (100000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
